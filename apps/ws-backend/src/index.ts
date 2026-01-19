b4c9326acefa76748b41b1079ca7d03a0af7d689
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-01-19 16:09:55 +0530

    feat: fetching token from params in ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -2,8 +2,17 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, request) {
   ws.on("error", console.error);
+
+  //get token from query params to authenticate users on ws connection
+  const url = request.url;
+  if(!url){
+    return;
+  }
+
+  const params = new URLSearchParams(url.split('?')[1]);
+  const token = params.get('token');
 
   ws.on("message", function message(data) {
     console.log("received: %s", data);
