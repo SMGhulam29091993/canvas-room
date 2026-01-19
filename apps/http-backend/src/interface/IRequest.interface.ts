1d88e43c4f8989919a604e136ac1c956f34f6c4e
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-01-19 22:25:16 +0530

    fix: removed folders as migrate to packages for centralize structure and code sharing

--- apps/http-backend/src/interface/IRequest.interface.ts
+++
@@ -1,6 +0,0 @@
-
-declare module "express"{
-    export interface Request {
-        userId?: string;
-    }
-}
--- apps/http-backend/src/interface/IResponse.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ApiResponse<T = any> {
-  statusCode: number;
-  success: boolean;
-  message: string;
-  data?: T | null;
-  error?: T | null;
-}
