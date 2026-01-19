d15a19b526dd30f3eef87c3f06b8d1d7db5be852
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-01-19 17:28:38 +0530

    feat: added valid token check for ws backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,9 @@
 import { WebSocketServer } from "ws";
+import * as jwt from 'jsonwebtoken';
+import {config} from "@repo/config";
 
 const wss = new WebSocketServer({ port: 8080 });
+type userId = string;
 
 wss.on("connection", function connection(ws, request) {
   ws.on("error", console.error);
@@ -13,6 +16,18 @@ wss.on("connection", function connection(ws, request) {
 
   const params = new URLSearchParams(url.split('?')[1]);
   const token = params.get('token');
+
+  if (!token) {
+    ws.close(1008, 'Unauthorized: No token provided');
+    return;
+  }
+  const decodedToken = jwt.verify(token, config.jwtSecret) as { userId: userId };
+  if (!decodedToken || !decodedToken.userId) {
+    ws.close(1008, 'Unauthorized: Invalid token');
+    return;
+  }
+
+
 
   ws.on("message", function message(data) {
     console.log("received: %s", data);
