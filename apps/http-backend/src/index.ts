18f81486c39e6012cb54fd8b31b82325d338537b
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-01-19 15:12:18 +0530

    feat:  added express server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,14 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-
-
 app.listen(PORT, (err) => {
-    if (err) {
-        console.error('Failed to start server:', err);
-        process.exit(1);
-    }
-    console.log(`Server is running on port ${PORT}`);
-})
-
-
+  if (err) {
+    console.error('Failed to start server:', err);
+    process.exit(1);
+  }
+  console.log(`Server is running on port ${PORT}`);
+});
