6e3136ff376b31bd5b21b8db0ba4f248b0798cca
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-02-21 21:11:59 +0530

    feat: updated the user schema ans types plus also added signup and signin controller

--- apps/http-backend/src/controller/auth.controller.ts
+++ apps/http-backend/src/controller/auth.controller.ts
@@ -1,3 +1,6 @@
+import { prismaClient } from '@repo/db/dist/index.js';
+import { sendResponse } from '@repo/lib/dist/helper.js';
+import bcrypt from 'bcrypt';
 import { NextFunction, Request, Response } from 'express';
 
 export const signUp = async (
@@ -6,10 +9,28 @@ export const signUp = async (
   next: NextFunction
 ) => {
   try {
-    const userData = req.body;
+    const { email, password, name } = req.body;
+    const existingUser = await prismaClient.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return sendResponse(res, 400, true, 'User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userData= {
+      email,
+      password: hashedPassword,
+    }
+
+    const newUser = await prismaClient.user.create({
+      data: userData,
+    });
+
+    if(!newUser) sendResponse(res, 500, false, 'Failed to create user');
+    
+    sendResponse(res, 201, false, 'User created successfully', { userId: newUser.id });
     
   } catch (error) {
-    
+    next(error);
   }
 };
 
@@ -18,4 +39,21 @@ export const signIn = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await prismaClient.user.findUnique({ where: { email } });
+    if (!user) {
+      return sendResponse(res, 400, true, 'Invalid email or password');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return sendResponse(res, 400, true, 'Invalid email or password');
+    } 
+
+    sendResponse(res, 200, false, 'Sign in successful', { userId: user.id });
+  } catch (error) {
+    next(error);
+  }
+};
--- packages/zod-dto/src/dtos/user.dto.ts
+++ packages/zod-dto/src/dtos/user.dto.ts
@@ -1,23 +1,25 @@
 import {z} from "zod";
 
 export const UserDto = z.object({
-    id: z.string().uuid(),
-    username: z.string().min(3).max(30),
-    name : z.string().min(1).max(100),
+    id: z.uuid(),
+    email: z.string().min(3).max(30),
+    name : z.string().min(1).max(100).optional(),
     password: z.string().min(8),
+    chats : z.array(z.number()).optional(),
+    rooms : z.array(z.number()).optional(),
     createdAt : z.date(),
     updatedAt : z.date().optional(),
 });
 
 const CreateUserDto = UserDto.omit({id: true, createdAt: true, updatedAt: true});
 
-const SignInDDto = z.object({
-    username: z.string().min(3).max(30),
+const SignInDto = z.object({
+    email: z.string().min(3).max(30),
     password: z.string().min(8),
 });
 
 export type IUserDto = z.infer<typeof UserDto>;
 export type ICreateUserDto = z.infer<typeof CreateUserDto>;
-export type ISignInDto = z.infer<typeof SignInDDto>;
+export type ISignInDto = z.infer<typeof SignInDto>;
 
-export {CreateUserDto, SignInDDto};
+export {CreateUserDto, SignInDto};
