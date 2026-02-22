a1ae4eb25a590dcd8e67830206992e4d60d1db5b
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-02-22 15:53:46 +0530

    feat: added auth routes

--- apps/http-backend/src/routes/api/v1/auth.route.ts
+++ apps/http-backend/src/routes/api/v1/auth.route.ts
@@ -1,5 +1,10 @@
 import express from 'express';
-
+import { zodValidator } from '@repo/zod-dto/dist/schema_validator/zod.validator.js';
+import { CreateUserDto, SignInDto } from '@repo/zod-dto/dist/dtos/user.dto.js';
+import { signIn, signUp } from 'src/controller/auth.controller.js';
 const route: express.Router = express.Router();
+
+route.post('/signup', zodValidator(CreateUserDto), signUp);
+route.post('/signin', zodValidator(SignInDto), signIn);
 
 export default route;
