b6fb4e402e454184fa6006e396fdf839178a1768
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-02-23 21:13:47 +0530

    feat: formatted and added routes and req body parser and morgan

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,41 @@
 import express from 'express';
-import { config } from './config/env.js';
+import { config } from '@repo/config';
+import morgan from 'morgan';
+import apiV1Route from './routes/api/v1/index.js';
+import { Logger } from '@repo/lib';
+import { prismaClient } from '@repo/db';
 
 const app = express();
 
 const PORT = config.port || 3001;
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.error('Failed to start server:', err);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", apiV1Route);
+
+
+
+const start = async () => {
+  try {
+    await prismaClient.$connect();
+    Logger.info("Connected to DB");
+
+    //starting the server on port 
+    app.listen(PORT, (err) => {
+      if (err) {
+        console.error('Failed to start server:', err);
+        process.exit(1);
+      }
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    Logger.error("Failed to connect with DB", error);
     process.exit(1);
   }
-  console.log(`Server is running on port ${PORT}`);
-});
+}
+
+start();
