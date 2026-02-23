686be9b99049638078cfa09a0993032bd2c65f91
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-02-23 21:15:50 +0530

    fix: added api/v/ to route

--- apps/http-backend/src/routes/api/v1/index.ts
+++ apps/http-backend/src/routes/api/v1/index.ts
@@ -3,10 +3,10 @@ import authRoute from './auth.route.js';
 
 const route: express.Router = express.Router();
 
-route.get('/', (req, res) => {
+route.get('/api/v1', (req, res) => {
   res.send('API v1 is working!');
 });
 
-route.use('/auth', authRoute);
+route.use('/api/v1/auth', authRoute);
 
 export default route;
