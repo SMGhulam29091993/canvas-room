45d5c2c081093f92803ac8b26e8e22e9ff1a7044
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-03-08 04:25:28 +0530

    feat: added a controller function to create db

--- apps/http-backend/src/controller/room.controller.ts
+++ apps/http-backend/src/controller/room.controller.ts
@@ -1,4 +1,32 @@
+import { prismaClient } from "@repo/db/dist/index.js";
+import { sendResponse } from "@repo/lib/dist/helper.js";
 import { NextFunction, Request, Response } from "express";
 
 
-export const createRoom = async (req : Request, res : Response, next : NextFunction) => {};
+
+export const createRoom = async (req : Request, res : Response, next : NextFunction) => {
+    try {
+        const {slug, capacity} = req.body;
+        if(!slug) {
+            return sendResponse(res, 400, false, "Slug is required");
+        } 
+        if (capacity === undefined || capacity === null) {
+            return sendResponse(res, 400, false, "Capacity is required");
+        }
+
+        const roomData = {
+            slug : slug,
+            ownerId : req.userId as string,
+            isAvailable : true,
+            capacity,
+        }
+
+        const createdRoom = await prismaClient.room.create({data : roomData});
+        if(!createdRoom) {
+            return sendResponse(res, 500, false, "Failed to create room");
+        }
+        sendResponse(res, 201, true, "Room created successfully", createdRoom);
+    } catch (error) {
+        next(error);
+    }
+};
