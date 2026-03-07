1371b455c11a632f78f7dec04af399a8bbaee6c1
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-03-07 17:03:51 +0530

    fix : immport the .env variable in env.ts

--- packages/config/src/env.ts
+++ packages/config/src/env.ts
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-dotenv.config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+dotenv.config({path : path.resolve(__dirname, '../../../.env')});
+
+const configENV = process.env.NODE_ENV || 'development';
+
+if (!configENV) console.log('No environment specified, using defaults...');
 
 export const config = {
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 4000,
     jwtSecret: process.env.JWT_SECRET || 'your-default-secret',
     refreshJwtSecret: process.env.REFRESH_JWT_SECRET || 'your-default-refresh-secret',
     accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY || '15m',
