ce0ca5bcc765b40ce66da9709c0799afc34cc9b1
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-03-08 04:27:12 +0530

    feat : added the route to create room

--- apps/http-backend/src/routes/api/v1/room.route.ts
+++ apps/http-backend/src/routes/api/v1/room.route.ts
@@ -1,6 +1,10 @@
+import { CreateRoomDto, zodValidator } from "@repo/zod-dto/dist/index.js";
 import express from "express";
+import { createRoom } from "src/controller/room.controller.js";
+import { authMiddleware } from "src/middleware/auth.middleware.js";
 
 const route : express.Router = express.Router();
 
+route.post("/create", authMiddleware, zodValidator(CreateRoomDto), createRoom);
 
 export default route;
